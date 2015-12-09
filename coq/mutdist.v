ca6fcce2f0fe0952a3318c68dc9de02ff0846614
Catalin Hritcu <catalin.hritcu@gmail.com>
2015-12-09 19:14:29 +0100

    First whack at defining SFA in terms to MD

--- coq/mutdist.v
+++ coq/mutdist.v
@@ -47,19 +47,76 @@ Definition mutual_distrust
                            link (permute pi ((map compile Ps)++az))
                         ~~ link (permute pi ((map compile Qs)++az))).
 
-(*Instance unstructured_components : context_language (finset component) (finset component) :=
-  {
-    cl_insert := union component;
-    cl_compatible := compatible;
-      cl_complete := fun P => complete (link P);
-      cl_stat_eq := fun P Q => forall A, compatible (A P);
-         cl_beh_eq: cl_program -> cl_program -> Prop;
-         cl_stat_eq_compatible_complete:
-      forall P Q,
-        cl_stat_eq P Q ->
-        (forall A,
-           cl_compatible A P /\ cl_complete (cl_insert A P) ->
-           cl_compatible A Q /\ cl_complete (cl_insert A Q))
+(* Instantiating structured full abstraction to obtain something
+   stronger than mutual distrust (we will use this to prove mutual
+   distrust for our compiler) *)
 
-  }.
-*)
+Definition admit {t:Type} : t. Admitted.
+Variable All : forall {a:Type} (P:a->Prop), list a -> Prop.
+Variable isInl : forall {a:Type} {b:Type}, (a+b) -> bool.
+Variable isSome : forall {a:Type}, option a -> bool.
+Coercion is_true : bool >-> Sortclass. (* Prop *)
+Variable inls : forall {a:Type} {b:Type}, list (a+b) -> list a.
+Variable inrs : forall {a:Type} {b:Type}, list (a+b) -> list b.
+Variable fsts : forall {a:Type} {b:Type}, list (a*b) -> list a.
+
+Section SFAfromMD.
+
+Context {interface: Type} {I: interface_language interface} {component program: Type}.
+
+(* We take both SFA programs and contexts to be "partial programs"
+   of the following type *)
+Definition pprog := list ((component*interface) + interface).
+
+(* Need interface equality to decide compatibility? At least in current formulation ... *)
+Variable beq_interface : interface -> interface -> bool.
+
+Fixpoint merge_compatible (p:pprog) (q:pprog) : option pprog :=
+  match p, q with
+  | [], [] => Some []
+  | inl (c,i1) :: p', inr i2 :: q'
+  | inr i2 :: p', inl (c,i1) :: q' =>
+      if beq_interface i1 i2 then
+        match merge_compatible p' q' with
+        | Some pq' => Some (inl (c,i1) :: pq')
+        | None     => None
+        end
+      else None
+  | _, _ => None
+  end.
+
+Instance context_lang_from_component_lang 
+  (compl : component_language I component program) :
+     context_language pprog pprog :=
+{
+  cl_insert c p := match merge_compatible c p with
+                   | Some cp => cp
+                   | None    => [] (* shouldn't happen *)
+                   end;
+  cl_compatible c p := isSome (merge_compatible c p);
+  cl_complete p := All isInl p;
+  cl_stat_eq p q := comp (inrs p ++ inrs q);
+  cl_beh_eq p q := beh_eq (link (fsts (inls p))) (link (fsts (inls q)));
+  cl_stat_eq_compatible_complete :=
+    admit (* <-- can't prove this without extra assumptions on comp *)
+}.
+
+Fixpoint context_has_shape (s : list (bool*interface)) (c : pprog) : bool :=
+  match s, c with
+  | [], [] => true
+  | (true,i1)::s', (inl (_,i2))::c'
+  | (false,i1)::s', (inr i2)::c'
+     => andb (beq_interface i1 i2) (context_has_shape s' c')
+  | _, _ => false
+  end.
+
+Instance structured_context_lang_from_component_lang 
+  (compl : component_language I component program) :
+     structured_context_language
+       (context_lang_from_component_lang compl)
+       (list (bool*interface)) := (* replace (bool*interface) with option interface
+                                  if dropping scl_program_has_shape *)
+{
+  scl_context_has_shape := context_has_shape;
+  scl_program_has_shape s p := admit (* the same, just flip all bools *)
+}.
