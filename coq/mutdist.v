a37877d4cc276111ee27de0d7117646efd1a8544
Catalin Hritcu <catalin.hritcu@gmail.com>
2015-12-14 16:46:21 +0100

    More on from mutual distrust to SFA / FA

--- coq/mutdist.v
+++ coq/mutdist.v
@@ -19,13 +19,14 @@ Class interface_language (interface: Type): Type :=
 Let comp
     {interface: Type}
     `{interface_language interface}
-    Is := compatible Is /\ complete Is.
+    (Is : list interface) := compatible Is /\ complete Is.
 
 Class component_language
       {interface: Type} (il: interface_language interface)
       (component: Type) (program: Type): Type :=
   {
     has_interface : component -> interface -> Prop;
+    (* + has_interface is a partial function from components to interfaces *)
     link: list component -> program;
     beh_eq: program -> program -> Prop
   }.
@@ -65,6 +66,7 @@ Coercion is_true : bool >-> Sortclass. (* Prop *)
 Variable somes : forall {a:Type}, list (option a) -> list a.
 Variable fsts : forall {a:Type} {b:Type}, list (a*b) -> list a.
 Variable snds : forall {a:Type} {b:Type}, list (a*b) -> list b.
+Variable fst_map : forall{a b c:Type}, (a->c) -> (a*b) -> (c*b).
 
 Section SFAfromMD.
 
@@ -74,37 +76,41 @@ Context {interface: Type} {I: interface_language interface} {component program: 
    of the following type *)
 Definition pprog := list (option (component*interface)).
 
-(* Need interface equality to decide compatibility? At least in current formulation ... *)
-Variable beq_interface : interface -> interface -> bool.
-
-Fixpoint merge_compatible (p:pprog) (q:pprog) : option pprog :=
+Fixpoint merge (p:pprog) (q:pprog) : option pprog :=
   match p, q with
   | [], [] => Some []
   | Some (c,i1) :: p', None :: q'
   | None :: p', Some (c,i1) :: q' =>
-    match merge_compatible p' q' with
-      | Some pq' => Some (Some (c,i1) :: pq')
-      | None     => None
+    match merge p' q' with
+    | Some pq' => Some (Some (c,i1) :: pq')
+    | None     => None
+    end
+  | None :: p', None :: q' =>
+    match merge p' q' with 
+    | Some pq' => Some (None :: pq')
+    | None     => None
     end
   | _, _ => None
   end.
+
+Definition interfaces_ok 
+  {compl : component_language I component program} (p:pprog) :=
+  All (fun PIP => match PIP with (P, IP) => has_interface P IP end) (somes p).
 
 Instance context_lang_from_component_lang
   (compl : component_language I component program) :
      context_language pprog pprog :=
 {
-  cl_insert c p := match merge_compatible c p with
+  cl_insert c p := match merge c p with
                    | Some cp => cp
                    | None    => [] (* shouldn't happen *)
                    end;
-  cl_compatible c p := match merge_compatible c p with
-                         | Some l =>
-                           All (fun PIP => match PIP with (P, IP) => has_interface P IP end)
-                               (somes l) /\
-                           compatible (snds (somes l))
-                         | None => False
-                       end;
-  cl_complete p := All isSome p /\ complete (snds (somes p));
+  cl_compatible c p :=
+    match merge c p with
+    | Some cp => interfaces_ok cp /\ compatible (snds (somes cp))
+    | None => False
+    end;
+  cl_complete p := All isSome p /\ interfaces_ok p /\ complete (snds (somes p));
   cl_stat_eq p q :=
     All2 (fun optPIP optQIQ =>
             match optPIP, optQIQ with
@@ -115,7 +121,7 @@ Instance context_lang_from_component_lang
             end) p q;
   cl_beh_eq p q := beh_eq (link (fsts (somes p))) (link (fsts (somes q)));
   cl_stat_eq_compatible_complete :=
-    admit (* <-- can't prove this without extra assumptions on comp *)
+    admit (* TODO *)
 }.
 
 Definition shape := list (bool*interface). (* replace (bool*interface) with option interface
@@ -123,19 +129,19 @@ Definition shape := list (bool*interface). (* replace (bool*interface) with opti
 Definition flip_shape (s : shape): shape :=
   List.map (fun bi => match bi with (b, i) => (negb b, i) end) s.
 
-Fixpoint context_has_shape (s : shape) (c : pprog) : bool :=
+Fixpoint context_has_shape
+    {compl : component_language I component program} (s : shape) (c : pprog) : Prop :=
   match s, c with
   | [], [] => true
-  | (true,i1)::s', (Some (_,i2))::c' =>
-    (* YJ: here, no has_interface check,
-           looks OK because it is already in the compatibility check *)
-    andb (beq_interface i1 i2) (context_has_shape s' c')
+  | (true,i1)::s', (Some (p,i2))::c' =>
+    has_interface p i2 /\ i1 = i2 /\ context_has_shape s' c'
   | (false,_)::s', None::c' =>
     context_has_shape s' c'
   | _, _ => false
   end.
 
-Definition program_has_shape (s : shape) (p : pprog) : bool :=
+Definition program_has_shape
+    {compl : component_language I component program}  (s : shape) (p : pprog) : Prop :=
   context_has_shape (flip_shape s) p.
 
 Instance structured_context_lang_from_component_lang
@@ -150,22 +156,53 @@ Instance structured_context_lang_from_component_lang
 
 End SFAfromMD.
 
+Section SFAimpliesMD.
+
+Context {interface: Type} {I : interface_language interface}
+        {hcomponent hprogram: Type} {lcomponent lprogram: Type}
+        (H: component_language I hcomponent hprogram)
+        (L: component_language I lcomponent lprogram)
+        (compile : hcomponent -> lcomponent).
+
+Definition map_compile (p:@pprog interface hcomponent) :
+                          @pprog interface lcomponent :=
+  List.map (option_map (fst_map compile)) p.
+
+Conjecture sfa_implies_md : 
+  structured_full_abstraction
+    (structured_context_lang_from_component_lang H)
+    (structured_context_lang_from_component_lang L)
+    map_compile ->
+  mutual_distrust H L compile.
+(* TODO: prove this *)
+
+End SFAimpliesMD.
+
 Section FAfromMD.
 
 Context {interface: Type} {I: interface_language interface} {component program: Type}.
 
 (* We take both FA programs and contexts to be "partial programs"
    of the following type *)
-Definition unstructured_pprog := list (component*interface).
+Definition uprog := list (component*interface).
+
+Definition ucontext :=
+  {n:nat & {lc:list (component*interface) & perm (length lc+n)}}%type.
+
+Fixpoint umerge (c:ucontext) (p:uprog) : uprog :=
+  let '(existT n (existT lc pi)) := c in
+  permute pi (lc ++ p).
 
 Instance unstructured_context_lang_from_component_lang
   (compl : component_language I component program) :
-     context_language unstructured_pprog unstructured_pprog :=
+     context_language uprog ucontext :=
 {
-  cl_insert c p := c ++ p;
-  cl_compatible c p := All (fun PIP => match PIP with (P, IP) => has_interface P IP end)
-                           (c ++ p) /\
-                       compatible (snds (c ++ p));
+  cl_insert := umerge;
+  cl_compatible c p := 
+    let '(existT n (existT lc pi)) := c in
+    n = length p /\
+    All (fun PIP => match PIP with (P, IP) => has_interface P IP end) (lc++p) /\
+    compatible (snds (permute pi (lc ++ p)));
   cl_complete p := complete (snds p);
   cl_stat_eq p q :=
     All2 (fun PIP QIQ =>
@@ -175,7 +212,13 @@ Instance unstructured_context_lang_from_component_lang
             end) p q;
   cl_beh_eq p q := beh_eq (link (fsts p)) (link (fsts q));
   cl_stat_eq_compatible_complete :=
-    admit (* <-- can't prove this without extra assumptions on comp *)
+    admit (* TODO *)
 }.
 
 End FAfromMD.
+
+(* TODO: using structured_context_lang_from_component_lang
+         and unstructured_context_lang_from_component_lang
+         we want to instantiate 
+         structured_full_abstraction_implies_full_abstraction
+*)
