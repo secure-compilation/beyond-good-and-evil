6756c5c88b0d231ed51778dda3810c444c51b732
Yannis Juglaret <yjuglaret@gmail.com>
2015-12-16 11:49:30 +0100

    Update languages

--- coq/rfj.v
+++ coq/rfj.v
@@ -1,68 +1,50 @@
 Require Import Coq.Lists.List.
 Import ListNotations.
 
+Require Import Coq.Relations.Relation_Operators.
+
 Require Import MutualDistrust.fullabst.
 Require Import MutualDistrust.mutdist.
 
 (* Define RFJ interfaces *)
 
-Parameter rfj_class_declaration_table: Set.
-Parameter rfj_object_declaration_table: Set.
-Parameter rfj_cdt_domain: rfj_class_declaration_table -> list nat.
-
-Definition rfj_declaration_table: Set :=
-  (rfj_class_declaration_table * rfj_object_declaration_table)%type.
-
-Record rfj_interface: Set :=
-  {
-    rfj_interface_imports: rfj_declaration_table;
-    rfj_interface_exports: rfj_declaration_table
-  }.
-
+Parameter rfj_interface: Set.
 Parameter rfj_compatible2: rfj_interface -> rfj_interface -> Prop.
-
+(* Compatibility is pair-wise compatibility. *)
 Inductive rfj_compatible: list rfj_interface -> Prop :=
 | rfj_compatible_nil: rfj_compatible []
 | rfj_compatible_cons: forall C Cs,
                          rfj_compatible Cs ->
                          Forall (rfj_compatible2 C) Cs ->
                          rfj_compatible (C :: Cs).
-
 Parameter rfj_complete: list rfj_interface -> Prop.
+
+Lemma rfj_compatible_cons_inv:
+  forall I Is, rfj_compatible (I :: Is) -> rfj_compatible Is.
+Proof.
+  intros I Is H.
+  now inversion H.
+Qed.
 
 Instance rfji: interface_language rfj_interface :=
   {
     compatible := rfj_compatible;
+    compatible_cons := rfj_compatible_cons_inv;
     complete := rfj_complete
   }.
 
-(* Define RFJ *)
+(* Define RFJ as a component language *)
 
-Parameter rfj_class_definition_table: Set.
-Parameter rfj_object_definition_table: Set.
-
-Definition rfj_definition_table: Set :=
-  (rfj_class_definition_table * rfj_object_definition_table)%type.
-
-Parameter rfj_has_interface:
-  rfj_definition_table -> rfj_interface -> Prop.
-
-Record rfj_component: Type :=
-  {
-    rfj_component_interface: rfj_interface;
-    rfj_component_definitions: rfj_definition_table;
-    (* restrict to well-formed/well-typed components *)
-    rfj_component_wf:
-      rfj_has_interface rfj_component_definitions rfj_component_interface
-  }.
-
+Parameter rfj_defs: Set.
+Definition rfj_component: Set := (rfj_interface * rfj_defs)%type.
 Definition rfj_program := list rfj_component.
-
 Parameter rfj_beh_eq: rfj_program -> rfj_program -> Prop.
 
-Instance rfj : component_language rfji rfj_component rfj_program :=
+Parameter rfj_has_interface: rfj_defs -> rfj_interface -> Prop.
+
+Instance rfj: component_language rfji rfj_component rfj_program :=
   {
-    has_interface := fun C I => rfj_component_interface C = I;
+    has_interface := fun C I => fst C = I /\ rfj_has_interface (snd C) I;
     link := id;
     beh_eq := rfj_beh_eq
   }.
--- coq/symbmach.v
+++ coq/symbmach.v
@@ -1,50 +1,52 @@
 Require Import Coq.Lists.List.
 Import ListNotations.
+Require Import Coq.Relations.Relation_Operators.
 
 Require Import MutualDistrust.fullabst.
 Require Import MutualDistrust.mutdist.
 Require Import MutualDistrust.rfj.
 
-Definition admit {t:Type} : t. Admitted.
-
 (* Define symbolic machine programs as a component language *)
 
-Parameter mem_word: Set.
-Parameter mem_tag: Set.
-
-Definition component_id := nat.
-Definition class_id := nat.
-Definition meth_id := nat.
-Definition obj_id := nat.
-
-Inductive mem_loc: Set :=
-| stackl : component_id -> mem_loc
-| methl : component_id -> class_id -> meth_id -> mem_loc
-| objl : component_id -> class_id -> obj_id -> mem_loc.
-
-(* Untagged symbolic programs *)
-
-Definition mem_region := list mem_word.
-
-Definition symb_prog := list (mem_loc * mem_region).
-
-(* Tagged symbolic programs *)
-
-Definition tagged_mem_region := list (mem_word * mem_tag).
-
-Definition tagged_symb_prog := list (mem_loc * tagged_mem_region).
-
-(* Components *)
-
+(* Components are not tagged: tags are provided by the linker *)
+Parameter symb_mem_word: Set.
+Parameter symb_mem_loc: Set.
+Definition symb_mem_region := list symb_mem_word.
+Definition symb_prog := list (symb_mem_loc * symb_mem_region).
 Definition symb_component := (rfj_interface * symb_prog)%type.
 
-(* Restricts the mem_locs to components that appear in rfj_interface *)
-Definition symb_prog_has_interface: symb_prog -> rfj_interface -> Prop :=
-  (* the program should define memory regions only for the components mentionned in the interface *)
+(* The linker sets tags *)
+Parameter symb_mem_tag: Set.
+Definition symb_mem_tagged_region := list (symb_mem_word * symb_mem_tag).
+Definition symb_tagged_prog := list (symb_mem_loc * symb_mem_tagged_region).
+Parameter symb_linker: list symb_component -> symb_tagged_prog.
 
-  (* when a location is missing, the corresponding memory regions will be filled with zeros and tagged *)
-  admit.
+(* The loader takes the result of the linker to a machine state. *)
+Parameter symb_machine_state: Set.
+Parameter symb_reduce: symb_machine_state -> symb_machine_state -> Prop.
+Parameter symb_loader: symb_tagged_prog -> symb_machine_state.
+Coercion symb_loader: symb_tagged_prog >-> symb_machine_state.
 
-(* Apply loader then look for equitermination. *)
-Instance symb_machine : component_language rfji symb_component tagged_symb_prog.
-Admitted.
+Notation "s --> s'" := (symb_reduce s s') (at level 50, no associativity).
+Notation "s -/-> s'" := (~ symb_reduce s s') (at level 50, no associativity).
+Notation "s -->* s'" := (clos_refl_trans _ symb_reduce s s')
+                        (at level 50, no associativity).
+
+Definition symb_stuck (s: symb_machine_state): Prop :=
+  forall (s': symb_machine_state), s -/-> s'.
+
+Definition symb_terminates (s: symb_machine_state) :=
+  exists s', s -->* s' /\ symb_stuck s'.
+
+Parameter symb_prog_has_interface : symb_prog -> rfj_interface -> Prop.
+(* The program should define memory regions only for the components *)
+(* mentioned in the interface. When a location is missing, the *)
+(* corresponding memory regions will be filled with zeros and *)
+(* tagged. *)
+
+Instance symb_machine : component_language rfji symb_component symb_tagged_prog :=
+  {
+    has_interface := fun C I => fst C = I /\ symb_prog_has_interface (snd C) I;
+    link := symb_linker;
+    beh_eq := fun P Q => (symb_terminates P <-> symb_terminates Q)
+  }.
