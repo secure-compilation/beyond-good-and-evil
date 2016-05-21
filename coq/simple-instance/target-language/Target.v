867f76605e1262bcddab7be32002144121bcea36
Eng Boris <engboris@hotmail.fr>
2016-05-21 19:37:09 +0200

    Small fix and add evaluation function for the target language.

--- coq/simple-instance/target-language/Target.v
+++ coq/simple-instance/target-language/Target.v
@@ -133,6 +133,14 @@ Definition call_exists (Is:program_interfaces)
   let import := get_import (nth C Is (0,0,[])) in
   (ble_nat C l) && negb (beq_nat C l) = true /\ (In (C,P) import).
 
+Definition boolean_call_exists (Is:program_interfaces)
+  (C:component_id) (P:procedure_id) : bool :=
+  let l := length Is in
+  let import := get_import (nth C Is (0,0,[])) in
+  (ble_nat C l) && negb (beq_nat C l) 
+    &&
+  (existsb (fun x => (fst x =? C) && (snd x =? P)) import).
+
 Reserved Notation "Is ;; E |- s '⇒' s'" (at level 40).
 Inductive step (Is:program_interfaces) (E:entry_points) : 
   state -> state -> Prop :=
@@ -175,11 +183,11 @@ Inductive step (Is:program_interfaces) (E:entry_points) :
     (update_mem C mem i1 i2 = mem') ->
     Is;;E |- (C,d,mem,reg,pc) ⇒ (C,d,mem',reg,pc+1)
   (* S_Jal *)
-  | S_Jal : forall mem C pc i i' r reg reg' d rra,
+  | S_Jal : forall mem C pc i i' r reg reg' d,
     (fetch_mem C mem pc = i) ->
     (decode i = Some (Jal r)) ->
     (fetch_reg r reg = i') ->
-    (update_reg rra (pc+1) reg = reg') ->
+    (update_reg r_ra (pc+1) reg = reg') ->
     Is;;E |- (C,d,mem,reg,pc) ⇒ (C,d,mem,reg',i')
   (* S_Call *)
   | S_Call : forall mem C pc i reg d d' C' P',
@@ -199,7 +207,7 @@ Inductive step (Is:program_interfaces) (E:entry_points) :
   | S_Return : forall mem C pc i i' d' reg d C',
     (fetch_mem C mem pc = i) ->
     (decode i = Some Return) ->
-    (d' = (PCall C' i')::d) ->
+    (d = (PCall C' i')::d') ->
     Is;;E |- (C,d,mem,reg,pc) ⇒ (C',d',mem,reg,i')
   (* S_BnzNZ *)
   | S_BnzNZ : forall mem C pc i i' r reg d,
@@ -215,12 +223,14 @@ Inductive step (Is:program_interfaces) (E:entry_points) :
     Is;;E |- (C,d,mem,reg,pc) ⇒ (C,d,mem,reg,pc+1)
   where "Is ;; E |- s '⇒' s'" := (step Is E s s').
 
-Inductive stuck_state : program_interfaces -> cfg -> Prop :=
+Inductive stuck_state : program_interfaces -> state -> Prop :=
   (* S_DecodingError *)
-  | S_DecodingError : forall Is i C d mem reg pc, 
+  | S_DecodingError : forall Is i C d mem reg pc,
+    (fetch_mem C mem pc = i) ->
     (decode i = None) -> stuck_state Is (C,d,mem,reg,pc)
   (* S_CallFail *)
   | S_CallFail : forall Is i C C' P' d mem reg pc,
+    (fetch_mem C mem pc = i) ->
     (decode i = Some (Call C' P')) ->
     ~(call_exists Is C' P') ->
     stuck_state Is (C,d,mem,reg,pc)
@@ -229,16 +239,154 @@ Inductive stuck_state : program_interfaces -> cfg -> Prop :=
     stuck_state Is (C,[],mem,reg,pc)
   (* S_Halt *)
   | S_Halt : forall Is i C d mem reg pc,
+    (fetch_mem C mem pc = i) ->
     (decode i = Some Halt) ->
     stuck_state Is (C,d,mem,reg,pc).
 
+Definition boolean_stuckstate (Is:program_interfaces) 
+  (cfg:state) : bool :=
+  match cfg with
+  (* S_EmptyCallStack *)
+  | (C,[],mem,reg,pc) => true
+  | (C,d,mem,reg,pc) =>
+    let i := fetch_mem C mem pc in
+    match (decode i) with
+    (* S_CallFail *)
+    | Some (Call C' P') =>
+      if (boolean_call_exists Is C' P') then
+        false
+      else
+        true
+    (* S_Halt *)
+    | Some Halt => true 
+    (* S_DecodingError & Non-stuck states *)
+    | _ => false
+    end   
+  end.
+
+(* _____________________________________ 
+          EVALUATION FUNCTION
+   _____________________________________ *)
+
+Definition basic_eval_step (Is:program_interfaces) (E:entry_points) 
+  (cfg:state) : state :=
+  match cfg with
+  | (C,d,mem,reg,pc) =>
+    let i := fetch_mem C mem pc in
+    match (decode i) with
+    (* S_Nop *)
+    | Some Nop =>
+      (C,d,mem,reg,pc+1)
+    (* S_Const *)
+    | Some (Const i' r) => 
+      let reg' := (update_reg r i' reg) in
+      (C,d,mem,reg',pc+1)
+    (* S_Mov *)
+    | Some (Mov r1 r2) =>
+      let reg' := (update_reg r2 (fetch_reg r1 reg) reg) in
+      (C,d,mem,reg',pc+1)
+    (* S_BinOp *)
+    | Some (BinOp op r1 r2 r3) =>
+      let reg' := (update_reg r3 (LLeval_binop 
+        (op, (fetch_reg r1 reg), (fetch_reg r2 reg))) reg) in
+      (C,d,mem,reg',pc+1)
+    (* S_Load *)
+    | Some (Load r1 r2) =>
+      let i1 := (fetch_reg r1 reg) in
+      let reg' := (update_reg r2 (fetch_mem C mem i1) reg) in
+      (C,d,mem,reg',pc+1)
+    (* S_Store *)
+    | Some (Store r1 r2) =>
+      let i1 := (fetch_reg r1 reg) in
+      let i2 := (fetch_reg r2 reg) in
+      let mem' := (update_mem C mem i1 i2) in
+      (C,d,mem',reg,pc+1)
+    (* S_Jal *)
+    | Some (Jal r) =>
+      let i' := (fetch_reg r reg) in
+      let reg' := (update_reg r_ra (pc+1) reg) in
+      (C,d,mem,reg',i')
+    (* S_Call *)
+    | Some (Call C' P') =>
+      if ((boolean_call_exists Is C' P') 
+          && 
+         (existsb (fun x => (fst x =? C') && (snd x =? P')) (get_import (nth C Is (0,0,[])))
+            ||
+        beq_nat C' C)) then
+        let d' := (PCall C (pc+1))::d in 
+        (C',d',mem,reg,fetch_entry_points C' P' E)
+      else
+        cfg
+    (* S_Jump *)
+    | Some (Jump r) =>
+      let i' := (fetch_reg r reg) in
+      (C,d,mem,reg,i')
+    (* S_Return *)
+    | Some Return =>
+      match d with
+      | [] => cfg
+      | (PCall C' i')::d' => (C',d',mem,reg,i')
+      end
+    (* S_BnzNZ *)
+    | Some (Bnz r i') =>
+      if negb (fetch_reg r reg =? 0) then
+        (C,d,mem,reg,pc+i')
+      else
+        (C,d,mem,reg,pc+1)
+    | _ => cfg
+    end
+  end.
+
+Fixpoint eval_step (Is:program_interfaces) (E:entry_points) 
+  (cfg:state) (limit:nat) : state :=
+  match limit with
+  | 0 =>
+    match cfg with
+    | (C,d,mem,reg,pc) => (C,d,mem,reg,pc+1)
+    end
+  | S n => 
+    match cfg with
+    (* S_EmptyCallStack *)    
+    | (C,[],mem,reg,pc) => (C,[],mem,reg,pc+1)
+    | (C,d,mem,reg,pc) =>
+      let i := fetch_mem C mem pc in
+      match (decode i) with
+      (* S_CallFail *)
+      | Some (Call C' P') =>
+        if (negb (boolean_call_exists Is C' P')) then
+          (C,d,mem,reg,pc+1)
+        else
+          eval_step Is E (basic_eval_step Is E cfg) n
+      (* S_Halt *)
+      | Some Halt => (C,d,mem,reg,pc+1)
+      (* S_DecodingError *)
+      | None => (C,d,mem,reg,pc+1)
+      (* Other cases *)
+      | _ => eval_step Is E (basic_eval_step Is E cfg) n
+      end
+    end
+  end.
+
 (* _____________________________________ 
           PROOF : DETERMINISM
-   _____________________________________ *)  
+   _____________________________________ *)
+
+Lemma step_eval_1step :
+  forall Is E cfg1 cfg2,
+  (Is;;E |- cfg1 ⇒ cfg2) -> eval_step Is E cfg1 1 = cfg2.
+Proof.
+Admitted.
 
 Theorem abstractmachine_determinism :
   forall Is E cfg cfg1 cfg2,
   (Is;;E |- cfg ⇒ cfg1) /\ (Is;;E |- cfg ⇒ cfg2) ->
   cfg1 = cfg2.
 Proof.
-Admitted.
+  intros. destruct H as [Hcfg1 Hcfg2].
+  apply step_eval_1step in Hcfg1.
+  apply step_eval_1step in Hcfg2.
+  rewrite <- Hcfg1. rewrite <- Hcfg2.
+  reflexivity.
+Qed.
+
+
