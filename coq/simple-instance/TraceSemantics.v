971350607320a6e8c3e4efa8c4896e8e5013d35d
Eng Boris <engboris@hotmail.fr>
2016-05-24 23:41:56 +0200

    Checkpoint.

--- coq/simple-instance/TraceSemantics.v
+++ coq/simple-instance/TraceSemantics.v
@@ -24,7 +24,9 @@ Inductive action : Type :=
   | Ext : external_action -> origin -> action
   | Int : internal_action -> origin -> action.
 
-(*Definition dual (t:label) :=
+Definition trace : Type := list action.
+
+Definition dual (t:trace) :=
   let f :=
     (fun alpha =>
      match alpha with
@@ -34,10 +36,42 @@ Inductive action : Type :=
      | Ext ea ContextOrigin => Ext ea ProgramOrigin
      end) in
   map f t.
+
+
+(* _____________________________________ 
+                  STATES
+   _____________________________________ *)
+
+Definition A_sigma :=
+  list component_id.
+
+Definition P_SIGMA : Type :=
+  admit.
+
+Definition A_SIGMA : Type := 
+  admit.
+
+Definition program_state : Type :=
+  (component_id *
+   P_SIGMA *
+   global_memory *
+   registers *
+   address).
+
+Definition context_state : Type :=
+  (component_id *
+   P_SIGMA *
+   global_memory).
+
+Inductive state_partial_view : Type :=
+  | ProgramControl : Target.state -> state_partial_view
+  | ContextControl : Target.state -> state_partial_view
+  | EXITED.
+
+(* ------- Definitions : Extra notations ------- *)
+
+(*Definition Top (PE:P_SIGMA) :=
+  match PE with
+  |  
 *)
-
-
-
-
-
 
