1a3dd77b00ac7f209c0ed846c8f1a33da75e85c9
Eng Boris <engboris@hotmail.fr>
2016-05-30 10:55:14 +0200

    Add a (temporary) definition of low-level context application and the definition of fully definedness for programs and contexts.

--- coq/simple-instance/Shape.v
+++ coq/simple-instance/Shape.v
@@ -69,17 +69,17 @@ Inductive context_has_shape :
   where "'CONTEXT_SHAPE' P ∈∘ s" := (context_has_shape P s).
 
 (* ------- Context application ------- *)
-Definition unify_components 
-  (K : option component * option component) : component :=
+Definition unify_option {X:Type} (default:X)
+  (K : option X * option X) : X :=
   match K with
   | (Some k1', None) => k1'
   | (None, Some k2') => k2'
-  | _ => (0,0,[],0,0,[])
+  | _ => default
   end.
 
 Definition context_application (A P : Source.partial_program) :
   Source.program :=
-  map unify_components (combine A P).
+  map (unify_option (0,0,[],0,0,[])) (combine A P).
 
 
 (* _____________________________________ 
@@ -115,5 +115,74 @@ Inductive ll_program_has_shape :
   where "'LL_PROGRAM_SHAPE' P ∈• s" := 
   (ll_program_has_shape P s).
 
-(* TODO *)
+Reserved Notation "'LL_CONTEXT_SHAPE' A ∈∘ s" (at level 40).
+Inductive ll_context_has_shape : 
+  Target.program -> shape -> Prop :=
+  | SH_LLcontext : forall Is_a mem E Is comps_id,
+    let f C x :=
+      match C with
+      | Some C' =>
+        match x with
+        | Some x' => C' =? x'
+        | None => false
+        end
+      | None => false
+      end
+    in
+    let not_in_compsid := 
+      fun (C:option component_id) =>
+        negb (existsb (f C) comps_id)
+    in
+    (forall i, (In i Is_a) -> 
+      ((nth (get_name i) Is (0,0,[])) = i)) ->
+    (dom_entry_points E = dom_global_memory mem) ->
+    (dom_global_memory mem = 
+      (dom2partial (compsInterface Is_a))) ->
+    (dom2partial (compsInterface Is_a) = 
+      filter not_in_compsid (dom2partial (compsInterface Is))) ->
+    LL_CONTEXT_SHAPE (Is_a, mem, E) ∈∘ (Is, comps_id)
+  where "'LL_CONTEXT_SHAPE' A ∈∘ s" := 
+  (ll_context_has_shape A s).
+
+(* ------- Context application ------- *)
+
+Definition LL_context_application (A P:Target.program) :
+  Target.program :=
+  match A with
+  | (Is_a, mem_a, E_a) =>
+    match P with
+    | (Is_p, mem_p, E_p) =>
+      (Is_a ++ Is_p,
+       mem_a ++ mem_p,
+       E_a ++ E_p)
+    end
+  end.
+
+
+(* _____________________________________ 
+            FULL DEFINEDNESS
+   _____________________________________ *)
+
+Inductive program_fully_defined : partial_program -> Prop :=
+  | FD_program : forall P (PC:Source.program) C s sh d K e,
+    (PROGRAM_SHAPE P ∈• sh) ->
+  ~(exists A, (CONTEXT_SHAPE A ∈∘ sh) ->
+    let PC := (context_application A P) in
+    (procbodies PC) ⊢ (initial_cfg_of PC) ⇒* (C,s,d,K,e) ->
+    undefined_cfg (procbodies PC) (C,s,d,K,e) ->
+    In C (compsComponent PC)) ->
+    program_fully_defined P.
+
+Inductive context_fully_defined : partial_program -> Prop :=
+  | FD_context : forall A (PC:Source.program) C s sh d K e,
+    (CONTEXT_SHAPE A ∈∘ sh) ->
+  ~(exists P, (PROGRAM_SHAPE P ∈• sh) ->
+    let PC := (context_application A P) in
+    (procbodies PC) ⊢ (initial_cfg_of PC) ⇒* (C,s,d,K,e) ->
+    undefined_cfg (procbodies PC) (C,s,d,K,e) ->
+    In C (compsComponent PC)) ->
+    context_fully_defined A.
+
+
+
 
