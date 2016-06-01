3a004c4294c3061ea7bf12b58cf3db58801ad283
Eng Boris <engboris@hotmail.fr>
2016-06-01 11:20:40 +0200

    Add lemma statement for 'canonicalization' and 'definability'.

--- coq/simple-instance/SCCProof.v
+++ coq/simple-instance/SCCProof.v
@@ -31,6 +31,39 @@ Inductive neq_progH : Source.program -> Source.program -> Prop :=
                  LEMMAS
    _____________________________________ *)
 
+(* ------- Canonicalization ------- *)
+Lemma canonicalization :
+  forall t s,
+  forall P, (PROGRAM_SHAPE P ∈• s) -> 
+    (program_fully_defined s P) ->
+    (in_Traces_p t (COMPILE_PROG P↓) s
+      <-> 
+     in_Traces_p (zetaC_t t) (COMPILE_PROG P↓) s).
+Proof.
+Admitted.
+
+(* ------- Definability / Trace mappping algorithm ------- *)
+Lemma definability :
+  forall t g1 s, 
+    t = zetaC_t t /\ (exists p, (LL_PROGRAM_SHAPE p ∈• s) -> 
+    in_Traces_p (t++[Ext g1 ProgramOrigin]) p s)
+      ->
+    exists A, 
+    (CONTEXT_SHAPE A ∈∘ s) /\ (context_fully_defined s A) /\
+    (* 1 *)
+    in_Traces_a t (COMPILE_PROG A↓) s /\
+    (* 2 *)
+    ((g1 <> End) -> 
+    in_Traces_a (t++[Ext g1 ProgramOrigin]++[Ext End ContextOrigin]) 
+      (COMPILE_PROG A↓) s) /\
+    (* 3 *)
+    forall g, ((zeta_gamma g) <> (zeta_gamma g1)) ->
+      forall g',
+      ~(in_Traces_a (t++[Ext g ProgramOrigin]++[Ext g' ContextOrigin]) 
+        (COMPILE_PROG A↓) s).
+Proof.
+Admitted.
+
 (* ------- Structured Full Abstraction ------- *)
 Theorem structured_full_abstraction :
   forall s,
